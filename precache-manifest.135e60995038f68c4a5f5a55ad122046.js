fd317302d70d37c560331852da4c1543e84334ea
prathi8081 <tjprathibha@gmail.com>
2020-08-01 14:37:43 +0530

    Updates

---
+++ precache-manifest.135e60995038f68c4a5f5a55ad122046.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e405be34a942f16edd6a996f582e8ba",
+    "url": "/frontend/index.html"
+  },
+  {
+    "revision": "c07328b8f96961fb8af0",
+    "url": "/frontend/static/js/2.7135a70b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/frontend/static/js/2.7135a70b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72fbe9bc20b561999b34",
+    "url": "/frontend/static/js/main.83e86941.chunk.js"
+  },
+  {
+    "revision": "747c638a0ed8091beb0a",
+    "url": "/frontend/static/js/runtime-main.12e52466.js"
+  }
+]);
